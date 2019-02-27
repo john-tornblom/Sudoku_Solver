ed91aed57ebb790b7b9824d82aa71228c4c9c256
John Törnblom <john.tornblom@liu.se>
2019-02-27 16:08:35 +0100

    csv: add comments describing the intentions of functions

--- src/sudoku_csv.ads
+++ src/sudoku_csv.ads
@@ -2,7 +2,10 @@ with Sudoku_Types; use Sudoku_Types;
 
 package Sudoku_CSV with SPARK_Mode is
    
-   function Decode(Text     : in String)        return Instance_Type;
+   -- Decode CSV-encoded text into a Sudoku instance
+   function Decode(Text : in String) return Instance_Type;
+   
+   -- Encode a Sudoku instance into CSV text
    function Encode(Instance : in Instance_Type) return String;
    
 end Sudoku_CSV;
