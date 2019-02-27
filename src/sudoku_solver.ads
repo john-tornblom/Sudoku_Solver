2b1755820e78939e5e062623a47760ed0598cdfd
John Törnblom <john.tornblom@liu.se>
2019-02-27 16:08:56 +0100

    solver: add comments describing the intentions of functions and procedures

--- src/sudoku_solver.ads
+++ src/sudoku_solver.ads
@@ -2,10 +2,16 @@ with Sudoku_Types; use Sudoku_Types;
 
 package Sudoku_Solver with SPARK_Mode is
    
+   -- Check if two cells are on the same row
    function Same_Row(I, J : Index_Type) return Boolean;
+   
+   -- Check if two cells are on the same column
    function Same_Col(I, J : Index_Type) return Boolean;
+   
+   -- Check if two cells are on the same box
    function Same_Box(I, J : Index_Type) return Boolean;
    
+   -- Check if a row is assigned with valid digit combinations according to Sudoku rules
    function Valid_Row(Instance: in Instance_Type; Row_Id : Group_Type) return Boolean with
      Post => 
        (if (for all Digit in Digit_Type'Range => 
@@ -13,7 +19,8 @@ package Sudoku_Solver with SPARK_Mode is
                  Digit = Instance((Row_Id * Board_Dimension) + I)))
         then Valid_Row'Result = True
         else Valid_Row'Result = False);
-
+	
+   -- Check if a column is assigned with valid digit combinations according to Sudoku rules
    function Valid_Col(Instance: in Instance_Type; Col_Id : Group_Type) return Boolean with
      Post => 
        (if (for all Digit in Digit_Type'Range => 
@@ -21,9 +28,11 @@ package Sudoku_Solver with SPARK_Mode is
                  Digit = Instance(Col_Id + (Board_Dimension * I))))
         then Valid_Col'Result = True
         else Valid_Col'Result = False);
-                                                                                      
+
+   -- Check if a box is assigned with valid digit combinations according to Sudoku rules
    function Valid_Box(Instance: in Instance_Type; Box_Id : Group_Type) return Boolean;
    
+   -- Check if a Sudoku instance is assigned with valid digit combinations
    function Is_Valid(Instance: in Instance_Type) return Boolean with
      Post => 
        (if (for all I in Group_Type'Range => Valid_Row(Instance, I) 
@@ -31,13 +40,15 @@ package Sudoku_Solver with SPARK_Mode is
             and Valid_Box(Instance, I))
         then Is_Valid'Result = True
         else Is_Valid'Result = False);
-
+	
+   -- Check all cells in a Sudoku instance are assigned a digit
    function Is_Complete(Instance: in Instance_Type) return Boolean with
      Post => 
        (if (for all I in Instance_Type'Range => Instance(I) /= 0)
         then Is_Complete'Result = True
         else Is_Complete'Result = False);
-   
+	
+   -- Complete a Sudoku instance with valid digit assignments
    procedure Solve(Instance: in out Instance_Type; Is_Solved: in out Boolean);
    
 end Sudoku_Solver;
